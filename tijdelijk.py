8a6c78c1eec4a07af47cc518d483d641a9a46bae
webni <demaeyer01@hotmail.com>
2025-06-25 20:08:50 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -10,6 +10,6 @@ reclame_tekst =f"vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts € 
 reclame_tekst2 = reclame_tekst[: 65]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3
-for el in reclame_tekst4:
+for el in reclame_tekst4 .lower():
     if el > 5:
 print(el)
