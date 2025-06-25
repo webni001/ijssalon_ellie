7bc82ba274cf0111f8a0a13e32f3e32c2c016257
webni <demaeyer01@hotmail.com>
2025-06-25 20:04:39 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,15 @@
+mijn_dictionary = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = mijn_dictionary["aardbei"] * 0.8
+
+reclame_tekst =f"vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts € {aanbieding}"
+reclame_tekst2 = reclame_tekst[: 65]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3
+for el in reclame_tekst4:
+    if el > 5:
+print(el)
